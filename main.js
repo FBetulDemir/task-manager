ff214729df27a69e2e967cffeb112c0e8e9d5417
FBetulDemir <fatmabetuld@gmail.com>
2024-04-09 09:27:44 +0200

    Fixed the listAllTasks function and listCompletedTasks function

--- main.js
+++ main.js
@@ -26,34 +26,55 @@ const taskManager = {
             console.log(this.tasks);
         }
     },
-    completeTask: function completeTask (taskID){
-        const task = parseInt(prompt("Please enter a description id to mark it as completed: "));
-        if (task.id === taskID){
+    // creating a function for completing the task
+    completeTask: function completeTask (){
+        const taskID = parseInt(prompt("Please enter a task ID to mark it as completed: "));
+        // checking taskID if it is not a number. 
+        if (isNaN(taskID)){
+            alert(`Please enter a valid task ID! ${taskID} can only be numbers!`);
+            this.completeTask();
+        }
+        //checking the tasks array if the given taskID exists in the list and assign it to a new variable. 
+        const task = this.tasks.find((task) => task.id === taskID);
+        if(task === undefined){
+            alert(`Please enter a valid task ID! ${taskID} cannot be found!`)
+        }
+        //if task is a valid id; then, register it as a completed task
+        else {
             task.completed = true;
-            console.log('The task is registered as completed.')
-        } else {
-            console.log (`Please enter a valid task id! ${taskID} cannot be found!`)
+            alert(`The task ${taskID} is registered as completed.`);
         }
     },
+    //creating a function to list all the tasks
     allTasks: function listAllTasks (listAll){
-        console.log(this.tasks);
+        console.log(`Listing all the tasks: ${JSON.stringify(this.tasks)}`);
+        alert(JSON.stringify(this.tasks));
     },
+    //creating the the function to list all the completed tasks
     completedTasks: function listCompletedTasks(){
-        if (task.completed = true) {
-            console.log("Here is the list of all completed tasks: ");
-            console.log(task.completed);
-        } else{
-            console.log('Unfortunately, there is no completed task to show.')
+        //an empty array for completed tasks
+        let allCompletedTasks = [];
+        let i = 0;
+        while(i < this.tasks.length){
+            const task = this.tasks[i];
+            if (task.completed === true){
+                allCompletedTasks.push(task)
+            }
+            i++
         }
+        console.log(`Listing all the completed tasks: ${JSON.stringify(allCompletedTasks)}`);
+        alert(JSON.stringify(allCompletedTasks));
     }
   };
   
 
-    
+//Menu function to show the user his/her choices    
 function menu(){
-    askUserName()
-    let menuOptions = parseInt(prompt('Menu: \n Please enter the right number :\n 1. Please enter 1 to add a task \n 2. Please enter 2 to complete a task \n 3. Please enter 3 to list all the tasks \n 4. Please enter 4 to list all the completed tasks \n 5. Please enter 5 to exit the task manager'));
-    while(menuOptions > 0 && menuOptions < 6){
+    //first get the user name and print a welcome message if the entry is valid
+    let userName = askUserName()
+    //Here is the menu with 5 different options/cases.
+    while(userName){
+        let menuOptions = parseInt(prompt('Menu: \n Please enter the right number :\n 1. Please enter 1 to add a task \n 2. Please enter 2 to complete a task \n 3. Please enter 3 to list all the tasks \n 4. Please enter 4 to list all the completed tasks \n 5. Please enter 5 to exit the task manager'));
         switch (menuOptions){
             case 1:
                 taskManager.addTask();
@@ -75,8 +96,6 @@ function menu(){
                 
         }
     }
-  
-    menuOptions = parseInt(prompt('Menu: \n 1. Please enter 1 to add a task \n 2. Please enter 2 to complete a task \n 3. Please enter 3 to list all the tasks \n 4. Please enter 4 to list all the completed tasks \n 5. Please enter 5 to exit the task manager \n Please enter the right number :'));
 }
 
 function askUserName(){ 
